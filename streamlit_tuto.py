455324cd3af50557dbfdafd3c1f533202ce4a84a
anaele-paris <anaele.cojan@gmail.com>
2024-09-23 17:55:25 +0200

    first commit displaying simple page with image

---
+++ streamlit_tuto.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.title("testing streamlit")
+st.header("header 1 - text")
+st.write("Hello ,let's learn how to build a streamlit app together")
+st.header("header 2 - image")
+st.image("cetaphil.jpg") # This function is used to display an image. 
+# st.audio(): This function is used to display an audio. 
+# st.video(): This function is used to display a video.
+# st.checkbox('yes')st.button('Click')st.radio('Pick your gender',['Male','Female'])
+# st.selectbox('Pick your gender',['Male','Female'])
+# st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
+# st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+# st.slider('Pick a number', 0,50)
