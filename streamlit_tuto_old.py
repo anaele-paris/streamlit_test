62b593dde2feeadd0f7ad8f170d23cf17f93caef
anaele-paris <anaele.cojan@gmail.com>
2024-09-23 19:07:56 +0200

    generated different pages and loan tuto

--- streamlit_tuto_old.py
+++ streamlit_tuto_old.py
@@ -6,10 +6,8 @@ st.header("header 2 - image")
 st.image("cetaphil.jpeg") # This function is used to display an image. 
 # st.audio(): This function is used to display an audio. 
 # st.video(): This function is used to display a video.
-# st.checkbox('yes')
-st.radio('Pick your gender',['Male','Female'])
-st.selectbox('Pick your gender',['Male','Female'])
-st.slider('Pick a number', 0,50)
-st.button('Click')
+# st.checkbox('yes')st.button('Click')st.radio('Pick your gender',['Male','Female'])
+# st.selectbox('Pick your gender',['Male','Female'])
 # st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
 # st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+# st.slider('Pick a number', 0,50)
